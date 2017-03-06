f3966495d6dd14fcf3ba821e2e16cb23ad620614
tanviramcl <tanviricbamcl@gmail.com>
2017-03-06 11:22:35 +0600

    commit

---
+++ UI/ReportViewer/StockDeclarationAfterPostedReportViewer.aspx.cs
@@ -0,0 +1,54 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UI_ReportViewer_StockDeclarationBeforePostedReportViewer : System.Web.UI.Page
+{
+    CommonGateway commonGatewayObj = new CommonGateway();
+    private ReportDocument rdoc = new ReportDocument();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["UserID"] == null)
+        {
+            Session.RemoveAll();
+            Response.Redirect("../../Default.aspx");
+        }
+
+        
+        DataTable dtReprtSource = new DataTable();
+        StringBuilder sbMst = new StringBuilder();
+        StringBuilder sbfilter = new StringBuilder();
+        sbfilter.Append(" ");
+        sbMst.Append("SELECT C.COMP_NM,B.COMP_CD, floor(P.TOT_NOS) tot_nos, p.f_cd,  f.f_name f_name,floor(P.TOT_NOS * B.BONUS / 100) share_alloted,B.FY, B.RECORD_DT, B.BOOK_TO, B.BONUS, B.RIGHT_APPR_DT, B.RIGHT, B.CASH, B.AGM, B.REMARKS, B.POSTED, b.pdate  FROM BOOK_CL B, COMP C, PFOLIO_BK P, fund f WHERE B.COMP_CD = P.COMP_CD AND B.COMP_CD = C.COMP_CD AND P.BAL_DT_CTRL = B.RECORD_DT and B.BONUS is not null and posted is not null and p.f_cd = f.f_cd order by p.f_cd");
+        sbMst.Append(sbfilter.ToString());
+        dtReprtSource = commonGatewayObj.Select(sbMst.ToString());
+        dtReprtSource.TableName = "StockDeclarationAFterPostedReport";
+        //dtReprtSource.WriteXmlSchema(@"D:\officialProject\2-13-2017\amclpmfs\UI\ReportViewer\Report\crtStockDeclarationAfterPostedReport.xsd");
+        if (dtReprtSource.Rows.Count > 0)
+        {
+            //string Path = Server.MapPath("~/Report/crtNegativeBalanceCheckReport.rpt");
+            string Path = Server.MapPath("Report/StockDeclarationAfterPostedReport.rpt");
+            rdoc.Load(Path);
+            rdoc.SetDataSource(dtReprtSource);
+            CR_StockDeclarationAfterPostedReport.ReportSource = rdoc;
+            CR_StockDeclarationAfterPostedReport.DisplayToolbar = true;
+            CR_StockDeclarationAfterPostedReport.HasExportButton = true;
+            CR_StockDeclarationAfterPostedReport.HasPrintButton = true;
+            //rdoc.SetParameterValue("prmtransTypeDetais", transTypeDetais);
+            rdoc = ReportFactory.GetReport(rdoc.GetType());
+
+        }
+        else
+        {
+            Response.Write("No Data Found");
+        }
+
+
+    }
+}
---
+++ UI/StockDeclarationAfterPostedReport.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Data;
+
+public partial class UI_BalancechekReport : System.Web.UI.Page
+{
+    DropDownList dropDownListObj = new DropDownList();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["UserID"] == null)
+        {
+            Session.RemoveAll();
+            Response.Redirect("../Default.aspx");
+        }
+      
+
+    }
+
+    protected void showButton_Click(object sender, EventArgs e)
+    {
+      
+
+        StringBuilder sb = new StringBuilder();
+        //sb.Append("window.open('ReportViewer/NegativeBalanceCheckReportViewer.aspx?p1date=" + p1date + "&p2date= " + p2date + "');");
+        //ClientScript.RegisterStartupScript(this.GetType(), "ReportViwer", sb.ToString(), true);
+        Response.Redirect("ReportViewer/StockDeclarationAfterPostedReportViewer.aspx");
+
+    }
+
+  
+
+
+
+
+
+    
+}
